2042786e266b8ac356b6a44bca509a43f4e7784b
unknown <nksayyad2900.com>
2020-12-03 08:07:58 +0530

    [NIJAM] Add:Check Employee Present or not

--- EmployeeWage/Program.cs
+++ EmployeeWage/Program.cs
@@ -7,6 +7,16 @@ namespace EmployeeWage
         static void Main(string[] args)
         {
 	    Console.WriteLine("Welcome To Employee Wage Computation");
+	    int isFullTime = 1;
+            int isPartTime = 2;
+            Random rand = new Random();
+            int rn = rand.Next(1, 3);
+            if (isFullTime == 1)
+                Console.WriteLine("Employee Is presnet FullTime");
+            else if (isPartTime == 2)
+                Console.WriteLine("Employee Is present PartTime");
+            else
+                Console.WriteLine("Employee Absent.!!!");
         }  
     }
 }
